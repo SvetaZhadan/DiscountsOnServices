ea3a8c8d06deeb024f374e3ef3629091864fed4d
SvetaZhadan <83220461+SvetaZhadan@users.noreply.github.com>
2022-07-05 20:43:03 +0300

    dinamic

--- app/js/index.js
+++ app/js/index.js
@@ -1,63 +1,130 @@
 //// ================================ Imports ======================================
+"use strict";
 //scss
-import 'bootstrap/dist/css/bootstrap-grid.css';
-import 'nouislider/dist/nouislider.css';
-import '../scss/index.scss';
+import "bootstrap/dist/css/bootstrap-grid.css";
+import "nouislider/dist/nouislider.css";
+import "../scss/index.scss";
 
 //js
-import $ from 'jquery';
-import Swiper from 'swiper';
-import { Fancybox, Carousel, Panzoom } from '@fancyapps/ui';
-import IMask from 'imask';
-import Choices from 'choices.js';
-import autoComplete from '@tarekraafat/autocomplete.js';
-import validate from 'validate.js';
-import ApexCharts from 'apexcharts';
-import { rippleEffect, Ripple } from 'data-ripple';
-import noUiSlider from 'nouislider';
-import Scrollbar from 'smooth-scrollbar';
-import { faqs } from './sourse.js';
+import $ from "jquery";
+import Swiper from "swiper";
+import { Fancybox, Carousel, Panzoom } from "@fancyapps/ui";
+import IMask from "imask";
+import Choices from "choices.js";
+import autoComplete from "@tarekraafat/autocomplete.js";
+import validate from "validate.js";
+import ApexCharts from "apexcharts";
+import { rippleEffect, Ripple } from "data-ripple";
+import noUiSlider from "nouislider";
+import Scrollbar from "smooth-scrollbar";
+import { faqs, info } from "./sourse.js";
 //// ================================ Code ======================================
+const platformpage = document.querySelector(".platform-page");
+const homePage=document.querySelector('.home-page');
+const platformpageHeight=platformpage.clientHeight;
+platformpage.style.height=0+'px';
+
+  const parent = document.querySelector(".home-page__choses");
+  info.forEach((item) => {
+    const cardTemp = parent.querySelector(".c-choose-temp");
+    setCard(cardTemp, item, ".c-choose");
+  });
+
+  const card = parent.querySelectorAll(".c-choose");
+  card.forEach((card, i) => {
+    const btn = card.querySelector(".b-button");
+    btn.onclick = function () {
+      homePage.style.display='none';
+      platformpage.style.height=400+'px'
+
+      const parent = document.querySelector(".platform-page__cards");
+      const cardTemp = parent.querySelector(".c-card-temp");
+      platformpage.querySelector(".name").textContent = info[i].name;
+
+      info[i].plans.forEach((item) => {
+          setCard(cardTemp, item, ".c-card");
+      });
+    };
+  });
+
+
+if (document.querySelector(".faqs")) {
+  faqs.forEach((item) => {
+    const parent = document.querySelector(".faq");
+    const catdTemp = parent.querySelector(".c-faqs-temp");
+    setCard(catdTemp, item, ".c-faqs");
+  });
+
+  const accordion = document.querySelectorAll(".c-faqs");
+
+  accordion.forEach((item) => {
+    const quest = item.querySelector(".question").parentNode;
+    const answ = item.querySelector(".answer");
+
+    const height = answ.clientHeight;
+    answ.style.height = 0 + "px";
+    quest.onclick = function () {
+      if (item.classList.contains("--active")) {
+        item.classList.remove("--active");
+        answ.style.height = 0 + "px";
+      } else {
+        item.classList.add("--active");
+        answ.style.height = height + "px";
+      }
+    };
+  });
+}
+
+if (document.querySelector("header")) {
+  const datatoggle = document.querySelectorAll("[data-toggle]");
+  const burgerMenu = document.querySelectorAll("[data-burger-menu]");
+  const header = document.querySelector("header");
+
+  datatoggle.forEach((btn) => {
+    const block = btn.parentNode;
+    btn.onclick = function () {
+      block.classList.contains("--active")
+        ? block.classList.remove("--active")
+        : block.classList.add("--active");
+    };
+  });
+
+  burgerMenu.forEach((btn) => {
+    btn.onclick = function () {
+      header.classList.contains("--active")
+        ? header.classList.remove("--active")
+        : header.classList.add("--active");
+    };
+  });
+}
 
 function makeClone(template, name) {
   return template.content.cloneNode(true).querySelector(name);
 }
 
-function setCard(template, item) {
-  const content = makeClone(template, '.home-page__faq ');
+function setCard(template, item, card) {
+  const content = makeClone(template, card);
 
   const keys = Object.keys(item);
 
   for (const key of keys) {
     const element = content.querySelector(`.${key}`);
-    element.textContent = item[key];
+
+    if (key === "plans") {
+      continue;
+    }
+    if (key === "subtitle") {
+      continue;
+    }
+    if (key === "link") {
+      element.href = "#" + item[key];
+    } else {
+      element.textContent = item[key];
+    }
+
+    if (key === "img") {
+      element.src = item[key];
+    }
   }
+  template.parentElement.appendChild(content);
 }
-
-const cardTemp = document.querySelector('.c-faqs-temp');
-const card=document.querySelectorAll('.c-faqs')
-
-console.log(card);
-faqs.forEach((items) => {
-  // console.log(items);
-  setCard(cardTemp,items)
-});
-
-const datatoggle = document.querySelectorAll('[data-toggle]');
-const burgerMenu = document.querySelectorAll('[data-burger-menu]');
-const header = document.querySelector('header');
-
-datatoggle.forEach((btn) => {
-  const block = btn.parentNode;
-  btn.onclick = function () {
-    console.log(block);
-
-    block.classList.contains('--active') ? block.classList.remove('--active') : block.classList.add('--active');
-  };
-});
-
-burgerMenu.forEach((btn) => {
-  btn.onclick = function () {
-    header.classList.contains('--active') ? header.classList.remove('--active') : header.classList.add('--active');
-  };
-});
--- app/js/sourse.js
+++ app/js/sourse.js
@@ -1,23 +1,111 @@
 const faqs = [
   {
     question: 'The set time has passed, but the invoice has not yet arrived, what should I do?',
-    answer: 'answer',
+    answer: 'With cash accounting, bookkeeping is simple: You dont record any transactions until money actually changes hands, Accounting Tools says. With accrual accounting, you record transactions as soon as you earn or owe money. If an invoice arrives before, or after, the goods youve paid for, recording the transaction properly requires a couple of extra accounting steps.',
   },
   {
     question: 'Can I change my Netflix account email and password after purchase?',
-    answer: 'answer',
+    answer: 'To update your Netflix email and password, start by going to Netflix and logging in. Next, go to the main menu by clicking the downward-facing triangle in the upper-right corner. Select Account to access your account overview page. Then, go to the Membership & Billing section, where you will find the option to reset your email address and/or password.',
   },
   {
     question: 'Can I add a phone number for password recovery?',
-    answer: 'answer',
+    answer: 'To make sure you can get back into your Google Account if you ever can’t sign in, add recovery information.',
   },
   {
     question: 'How soon will I receive the account information I ordered?',
-    answer: 'answer',
+    answer: 'The order process typically takes 3-5 business days. Youll receive an email once your order status changes to “Shipped” or if further action ',
   },
   {
     question: 'How to pay?',
     answer: 'Payment options we currently have: PayPal, Bitcoin.',
   },
 ];
-export { faqs };
+
+const info=[
+  {
+    name: 'Netflix',
+    description:'Netflix can be accessed via web browsers or via application software installed on smart TVs, set-top boxes connected to televisions, tablet computers, smartphones.',
+    img:'../assets/images/ntflx.png',
+    link:'netflix',
+    subtitle:'n123',
+    plans:[
+      {
+        name:'Basic',
+        items:[
+          'Ad-free music listening','Play anywhere - even offline','On-demand playback' 
+        ],
+        price:45,
+      },
+      {
+        name:'Standart',
+        items:[
+          'Standart Ad-free music listening','Play anywhere - even offline','On-demand playback' 
+        ],
+        price:60,
+      },
+      {
+        name:'Premium',
+        items:[
+          'Premium Ad-free music listening','Play anywhere - even offline','On-demand playback' 
+        ],
+        price:80,
+      },
+    ]
+  },
+
+  {
+    name: 'YouTube Premium',
+    description:'The service provides ad-free access to content across the service, as well as access to premium YouTube',
+    img:'../assets/images/ytb.png',
+    link:'youtube',
+    subtitle:'y123',
+    plans:[
+      {
+        name:'Music',
+        items:[
+          'Listen to music without ads, in the background and offline',
+        ],
+        price:60,
+      },
+      {
+        name:'Premium',
+        items:[
+          'YouTube and YouTube Music without ads, in the background and offline',
+        ],
+        price:80,
+      },
+    ]
+  },
+  {
+    name: 'Spotify',
+    description:'Spotify offers digital copyright restricted recorded music and podcasts, including more than 82 million songs, from record labels and media companies.',
+    img:'../assets/images/sptf.png',
+    link:'spotify',
+    subtitle:'s123',
+    plans:[
+      {
+        name:'Individual',
+        items:[
+          'Ad-free music listening','Play anywhere - even offline','On-demand playback' 
+        ],
+        price:47,
+      },
+      {
+        name:'Duo',
+        items:[
+          '2 Premium accounts for a couple under one roof','Ad-free music listening, play offline, on-demand playback',
+        ],
+        price:57,
+      },
+      {
+        name:'Family',
+        items:[
+          '6 Premium accounts for family members living under one roof','Block explicit music','Ad-free music listening, play offline, on-demand playback', 
+        ],
+        price:77,
+      },
+    ]
+  },
+  
+];
+export { faqs, info };
